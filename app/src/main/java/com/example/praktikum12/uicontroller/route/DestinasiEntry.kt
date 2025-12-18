d5f7651b86873baf7a8d8423c2ca1d1d9e5c67e7
kiieeaa <kiyaadinda5@gmail.com>
2025-12-18 20:44:16 +0700

    Objek navigasi yang mendefinisikan rute untuk halaman tambah (entry) data siswa

---
+++ app/src/main/java/com/example/praktikum12/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,6 @@
+package com.example.praktikum12.uicontroller.route
+import com.example.praktikum12.R
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
