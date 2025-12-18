429d134ee5f0c003cecc6f1b3bfa9ebc5ac458d9
kiieeaa <kiyaadinda5@gmail.com>
2025-12-18 20:43:29 +0700

    Destinasi navigasi untuk layar detail siswa beserta rute dan argumen ID siswa

---
+++ app/src/main/java/com/example/praktikum12/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum12.uicontroller.route
+
+import com.example.praktikum12.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_details"
+    override val titleRes = R.string.detail_siswa
+    const val siswaIdArg = "itemId"
+    val routeWithArgs = "$route/{$siswaIdArg}"
+}
