836514886958ff1a2e65210a1b14712a999c2bf4
kiieeaa <kiyaadinda5@gmail.com>
2025-12-18 20:44:41 +0700

    Objek navigasi yang merepresentasikan halaman utama (home) aplikasi dengan judul nama aplikasi

---
+++ app/src/main/java/com/example/praktikum12/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.praktikum12.uicontroller.route
+
+import com.example.praktikum12.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
