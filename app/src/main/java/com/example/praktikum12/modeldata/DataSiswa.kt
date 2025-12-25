39bf4c1ff7d4ba0ceba5cd805ce99a4641a10372
kiieeaa <kiyaadinda5@gmail.com>
2025-12-25 17:27:06 +0700

    Mengubah DataSiswa menjadi DetailSiswa (biasanya untuk keperluan form edit)

--- app/src/main/java/com/example/praktikum12/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/praktikum12/modeldata/DataSiswa.kt
@@ -34,3 +34,10 @@ fun DataSiswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UISt
     detailSiswa = this.toDetailSiswa(),
     isEntryValid = isEntryValid
 )
+
+fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
