a09c530d815574e421c03f4efbac626644226359
kiieeaa <kiyaadinda5@gmail.com>
2025-12-25 17:32:18 +0700

    Argumen ID siswa yang dikirim saat navigasi ke halaman edit

--- app/src/main/java/com/example/praktikum12/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/praktikum12/uicontroller/route/DestinasiEdit.kt
@@ -5,6 +5,7 @@ import com.example.praktikum12.R
 object DestinasiEdit : DestinasiNavigasi {
     override val route = "item_edit"
     override val titleRes = R.string.edit_siswa
+    // Pastikan nama variabel ini siswaIdArg
     const val siswaIdArg = "itemId"
     val routeWithArgs = "$route/{$siswaIdArg}"
 }
