eadd9d7d40df2eec5cda9128437e9304d42ebcc7
kiieeaa <kiyaadinda5@gmail.com>
2025-12-25 17:49:00 +0700

    Mengambil ID siswa dari SavedStateHandle untuk menentukan data yang akan diedit

--- app/src/main/java/com/example/praktikum12/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/praktikum12/viewmodel/EditViewModel.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.praktikum12.modeldata.DetailSiswa
 import com.example.praktikum12.modeldata.UIStateSiswa
 import com.example.praktikum12.modeldata.toDataSiswa
+import com.example.praktikum12.modeldata.toUIStateSiswa
 import com.example.praktikum12.repositori.RepositoryDataSiswa
 import com.example.praktikum12.uicontroller.route.DestinasiEdit
 import kotlinx.coroutines.launch
@@ -17,44 +18,34 @@ class EditViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryDataSiswa: RepositoryDataSiswa
 ) : ViewModel() {
-    // PASTIKAN NAMA VARIABEL ADALAH siswaUiState
-    var siswaUiState by mutableStateOf(UIStateSiswa())
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
-    private val siswaId: Int = checkNotNull(savedStateHandle[DestinasiEdit.siswaIdArg])
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiEdit.siswaIdArg])
 
     init {
         viewModelScope.launch {
-            val siswa = repositoryDataSiswa.getSiswaById(siswaId)
-            siswaUiState = UIStateSiswa(
-                detailSiswa = DetailSiswa(
-                    id = siswa.id,
-                    nama = siswa.nama,
-                    alamat = siswa.alamat,
-                    telpon = siswa.telpon,
-                    nim = siswa.nim
-                ),
-                isEntryValid = true
-            )
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa).toUIStateSiswa(true)
         }
     }
 
     fun updateUiState(detailSiswa: DetailSiswa) {
-        siswaUiState = UIStateSiswa(
-            detailSiswa = detailSiswa,
-            isEntryValid = validasiInput(detailSiswa)
-        )
+        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
 
-    private fun validasiInput(uiState: DetailSiswa): Boolean {
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
         return with(uiState) {
-            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank() && nim.isNotBlank()
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
 
-    suspend fun updateSiswa() {
-        if (validasiInput(siswaUiState.detailSiswa)) {
-            repositoryDataSiswa.updateSiswa(siswaId, siswaUiState.detailSiswa.toDataSiswa())
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            try {
+                repositoryDataSiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
